aea9de46be5881f4b1ef3a5a52b1b3b964866188
Fidores <45700014+Fidores@users.noreply.github.com>
2020-08-04 22:04:35 +0200

    Added rippe effect

---
+++ src/app/directives/ripple/ripple.directive.ts
@@ -0,0 +1,26 @@
+import {
+	Directive,
+	HostListener,
+	HostBinding,
+	ElementRef,
+} from '@angular/core';
+
+@Directive({
+	selector: '[ripple], .btn',
+})
+export class RippleDirective {
+	constructor(private readonly host: ElementRef<HTMLElement>) {}
+
+	@HostListener('mousedown', ['$event']) positionRipple($event: MouseEvent) {
+		if (this.isRippling) return null;
+		const { offsetX, offsetY } = $event;
+
+		this.host.nativeElement.style.setProperty('--ripple-x-pos', `${offsetX}px`);
+		this.host.nativeElement.style.setProperty('--ripple-y-pos', `${offsetY}px`);
+
+		this.isRippling = true;
+		setTimeout(() => (this.isRippling = false), 500);
+	}
+	@HostBinding('class.ripple--is-rippling') private isRippling: boolean = false;
+	@HostBinding('class.ripple') isEnabled: boolean = true;
+}
