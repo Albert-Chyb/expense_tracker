873814ef8779016c7d3d3b192dfe2610a449e4cc
Fidores <45700014+Fidores@users.noreply.github.com>
2020-08-15 21:39:14 +0200

    Added clues images in case no data is available

--- src/app/pages/manage-groups/manage-groups.component.ts
+++ src/app/pages/manage-groups/manage-groups.component.ts
@@ -1,5 +1,5 @@
 import { TransactionsGroupsService } from 'src/app/services/transactions-groups/transactions-groups.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ITransactionGroup } from './../../common/models/group';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,30 +9,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageGroupsComponent implements OnInit {
 	constructor(private readonly _groups: TransactionsGroupsService) {}
-
-	groups: ITransactionGroup[] = [
-		{
-			name: 'Pensja',
-			icon: {
-				name: 'fa-coins',
-				type: 'fas',
-			},
-		},
-		{
-			name: 'Jedzenie',
-			icon: {
-				name: 'fa-utensils',
-				type: 'fas',
-			},
-		},
-		{
-			icon: {
-				type: 'fas',
-				name: 'fa-paw',
-			},
-			name: 'Weterynarz',
-		},
-	];
 
 	groups$: Observable<ITransactionGroup[]>;
 
