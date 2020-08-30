4294c7486e374a6fcf95d482f1538f6096a83a79
Fidores <45700014+Fidores@users.noreply.github.com>
2020-08-30 23:01:50 +0200

    Switched to correct time zone

--- functions/src/autoEndPeriod.ts
+++ functions/src/autoEndPeriod.ts
@@ -11,8 +11,8 @@ const db = firestore();
 
 export const autoEndPeriod = functions.pubsub
 	.schedule('0 0 * * *')
+	.timeZone('Europe/Warsaw')
 	.onRun(async context => {
-		console.log('Tried to auto end periods');
 		const users = await db.collection('users').listDocuments();
 
 		users.forEach(async userDoc => {
