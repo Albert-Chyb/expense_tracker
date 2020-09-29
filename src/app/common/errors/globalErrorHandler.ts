82e45bbd4b8094051f85cbe731bbaed965c7295b
Fidores <45700014+Fidores@users.noreply.github.com>
2020-09-29 21:07:55 +0200

    Added notification for user about unexpected error

--- src/app/common/errors/globalErrorHandler.ts
+++ src/app/common/errors/globalErrorHandler.ts
@@ -1,12 +1,17 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorsService } from './../../services/errors/errors.service';
+import { ErrorHandler, Injectable } from '@angular/core';
 
 import { environment } from './../../../environments/environment';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
+	constructor(private readonly _errors: ErrorsService) {}
+
 	handleError(error: any): void {
 		if (!environment.production) {
 			console.error(error);
 		} else {
+			this._errors.notifyUser('An error occurred');
 			/*
                 Here, an unhandled error would be automatically reported to some service.
             */
