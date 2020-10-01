90010ab1b840d433bc53214bb9356268238e1a65
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-01 21:54:56 +0200

    Added dynamic calculation of ripple size

--- src/app/directives/ripple/ripple.directive.ts
+++ src/app/directives/ripple/ripple.directive.ts
@@ -3,14 +3,17 @@ import {
 	ElementRef,
 	HostBinding,
 	HostListener,
+	OnInit,
 	Renderer2,
 	RendererStyleFlags2,
 } from '@angular/core';
 
+type RippleProperty = 'x' | 'y' | 'size' | 'duration';
+
 @Directive({
 	selector: '[ripple], .btn:not(.no-ripple), .ripple',
 })
-export class RippleDirective {
+export class RippleDirective implements OnInit {
 	constructor(
 		private readonly _host: ElementRef<HTMLElement>,
 		private readonly _renderer: Renderer2
@@ -18,30 +21,38 @@ export class RippleDirective {
 
 	private isRippling: boolean = false;
 	private readonly ripplingClass = 'ripple--is-rippling';
+	private readonly animationDuration = 500;
 
 	@HostListener('mousedown', ['$event']) positionRipple($event: MouseEvent) {
 		if (this.isRippling) return null;
 		const { offsetX, offsetY } = $event;
 		const { nativeElement } = this._host;
+		const { width, height } = this._host.nativeElement.getBoundingClientRect();
+		const rippleSize = width > height ? width : height;
 
-		this._renderer.setStyle(
-			nativeElement,
-			'--ripple-x-pos',
-			`${offsetX - 25}px`,
-			RendererStyleFlags2.DashCase
-		);
-		this._renderer.setStyle(
-			nativeElement,
-			'--ripple-y-pos',
-			`${offsetY - 25}px`,
-			RendererStyleFlags2.DashCase
-		);
+		this.setRippleProperty('x', `${offsetX - rippleSize / 2}px`);
+		this.setRippleProperty('y', `${offsetY - rippleSize / 2}px`);
+		this.setRippleProperty('size', `${rippleSize}px`);
+
 		this._renderer.addClass(nativeElement, this.ripplingClass);
 
 		setTimeout(() => {
 			this._renderer.removeClass(nativeElement, this.ripplingClass);
 			this.isRippling = false;
-		}, 500);
+		}, this.animationDuration);
 	}
 	@HostBinding('class.ripple') isEnabled: boolean = true;
+
+	ngOnInit(): void {
+		this.setRippleProperty('duration', `${this.animationDuration}ms`);
+	}
+
+	private setRippleProperty(varName: RippleProperty, value: string): void {
+		this._renderer.setStyle(
+			this._host.nativeElement,
+			`--ripple-${varName}`,
+			value,
+			RendererStyleFlags2.DashCase
+		);
+	}
 }
