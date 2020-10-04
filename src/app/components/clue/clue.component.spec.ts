1e518867efc4681fd759a6d01d236cba41ef401c
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-04 22:44:05 +0200

    Added tests for clue component

--- src/app/components/clue/clue.component.spec.ts
+++ src/app/components/clue/clue.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ClueComponent } from './clue.component';
 
-describe('ClueComponent', () => {
-  let component: ClueComponent;
-  let fixture: ComponentFixture<ClueComponent>;
+fdescribe('ClueComponent', () => {
+	let component: ClueComponent;
+	let fixture: ComponentFixture<ClueComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ClueComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ClueComponent],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ClueComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ClueComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should have `clue--fixed` class when isFixed property is set to true', () => {
+		const de = fixture.debugElement.query(By.css('.clue'));
+		const el: HTMLElement = de.nativeElement;
+
+		component.isFixed = true;
+		fixture.detectChanges();
+		expect(el.classList.contains('clue--fixed')).toBeTruthy();
+
+		component.isFixed = false;
+		fixture.detectChanges();
+		expect(el.classList.contains('clue--fixed')).toBeFalsy();
+	});
+
+	it('should have src attribute on img element the same as in the imgSrc property', () => {
+		const de = fixture.debugElement.query(By.css('.clue__image'));
+		const el: HTMLImageElement = de.nativeElement;
+
+		component.imgSrc = 'aaa';
+		fixture.detectChanges();
+		expect(el.src).toBe(`${location.protocol}//${location.host}/aaa`);
+	});
 });
