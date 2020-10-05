0130a26bfc5f2589a5569a01795c69078084a5e1
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-05 14:45:29 +0200

    Added tests

--- src/app/components/group-icon/group-icon.component.spec.ts
+++ src/app/components/group-icon/group-icon.component.spec.ts
@@ -3,23 +3,55 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GroupIconComponent } from './group-icon.component';
 
 describe('GroupIconComponent', () => {
-  let component: GroupIconComponent;
-  let fixture: ComponentFixture<GroupIconComponent>;
+	let component: GroupIconComponent;
+	let fixture: ComponentFixture<GroupIconComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GroupIconComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [GroupIconComponent],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GroupIconComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(GroupIconComponent);
+		component = fixture.componentInstance;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		component.type = 'neutral';
+		component.icon = {
+			name: 'car',
+			type: 'fab',
+		};
+
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('classes', () => {
+		it('should contain group icon type', () => {
+			component.type = 'someType' as any;
+
+			expect(component.classes).toContain('group-icon--someType');
+		});
+
+		it('should contain icon type', () => {
+			component.icon = {
+				name: '' as any,
+				type: 'someType' as any,
+			};
+
+			expect(component.classes).toContain('someType');
+		});
+
+		it('should contain icon name', () => {
+			component.icon = {
+				name: 'someName' as any,
+				type: '' as any,
+			};
+
+			expect(component.classes).toContain('someName');
+		});
+	});
 });
