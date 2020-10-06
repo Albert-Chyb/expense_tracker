f586c034ef392115fdcb45f147129e1b7680fb8f
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-06 21:56:54 +0200

    Added tests

--- src/app/components/main-header/main-header.component.spec.ts
+++ src/app/components/main-header/main-header.component.spec.ts
@@ -1,25 +1,83 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { first } from 'rxjs/operators';
+import { environment } from './../../../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+	async,
+	ComponentFixture,
+	fakeAsync,
+	TestBed,
+	tick,
+} from '@angular/core/testing';
 
 import { MainHeaderComponent } from './main-header.component';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
-describe('MainHeaderComponent', () => {
-  let component: MainHeaderComponent;
-  let fixture: ComponentFixture<MainHeaderComponent>;
+@Component({ template: '' })
+class EmptyComponent1 {}
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MainHeaderComponent ]
-    })
-    .compileComponents();
-  }));
+@Component({ template: '' })
+class EmptyComponent2 {}
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+fdescribe('MainHeaderComponent', () => {
+	let component: MainHeaderComponent;
+	let fixture: ComponentFixture<MainHeaderComponent>;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [MainHeaderComponent],
+			imports: [
+				AngularFireModule.initializeApp(environment.firebase),
+				RouterTestingModule.withRoutes([
+					{
+						path: 'route1',
+						component: EmptyComponent1,
+						data: {
+							name: 'a',
+						},
+					},
+					{
+						path: 'route2',
+						component: EmptyComponent2,
+						data: {
+							name: 'b',
+						},
+					},
+				]),
+			],
+		})
+			.overrideComponent(MainHeaderComponent, {
+				set: { changeDetection: ChangeDetectionStrategy.Default },
+			})
+			.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MainHeaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should display current`s route name', async () => {
+		const router: Router = TestBed.inject(Router);
+		const de = fixture.debugElement.query(By.css('.header__heading'));
+		const el: HTMLHeadingElement = de.nativeElement;
+
+		component.ngOnInit();
+		fixture.detectChanges();
+
+		component.pageName$.subscribe(name => {
+			fixture.detectChanges();
+
+			expect(el.textContent).toBe(name);
+		});
+
+		await router.navigateByUrl('/route2');
+	});
 });
