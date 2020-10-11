e5722dcc2b3981096adef59c7d8941687e587d31
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-11 20:27:22 +0200

    Added tests

--- src/app/guards/data-available/data-available.guard.spec.ts
+++ src/app/guards/data-available/data-available.guard.spec.ts
@@ -1,15 +1,71 @@
+import { Pages } from './../../common/routing/routesUrls';
+import { of, Observable } from 'rxjs';
+import { UserService } from './../../services/user/user.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from './../../../environments/environment';
+import { AngularFireModule } from '@angular/fire';
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { DataAvailableGuard } from './data-available.guard';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({})
+class TestComponent {}
 
 describe('DataAvailableGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DataAvailableGuard]
-    });
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [DataAvailableGuard],
+			imports: [
+				AngularFireModule.initializeApp(environment.firebase),
+				RouterTestingModule.withRoutes([
+					{
+						path: 'setup-account',
+						component: TestComponent,
+					},
+				]),
+			],
+		});
+	});
 
-  it('should ...', inject([DataAvailableGuard], (guard: DataAvailableGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+	it('should give access to users that created initialization data', inject(
+		[DataAvailableGuard, UserService],
+		(guard: DataAvailableGuard, user: UserService) => {
+			spyOnProperty(user, 'hasCreatedData$').and.returnValue(of(true));
+
+			(guard.canActivate(null, null) as Observable<boolean>).subscribe(
+				hasAccess => {
+					expect(hasAccess).toBeTruthy();
+				}
+			);
+		}
+	));
+
+	it('should refuse access to users that did not create initialization data', inject(
+		[DataAvailableGuard, UserService],
+		(guard: DataAvailableGuard, user: UserService) => {
+			spyOnProperty(user, 'hasCreatedData$').and.returnValue(of(false));
+
+			(guard.canActivate(null, null) as Observable<boolean>).subscribe(
+				hasAccess => {
+					expect(hasAccess).toBeFalsy();
+				}
+			);
+		}
+	));
+
+	it('should redirect user to setup-account page if user did not create initialization data', inject(
+		[DataAvailableGuard, UserService, Router],
+		(guard: DataAvailableGuard, user: UserService, router: Router) => {
+			spyOnProperty(user, 'hasCreatedData$').and.returnValue(of(false));
+			const spy = spyOn(router, 'navigateByUrl').and.returnValue(
+				Promise.resolve(true)
+			);
+
+			(guard.canActivate(null, null) as Observable<boolean>).subscribe(() => {
+				expect(spy).toHaveBeenCalledWith(Pages.SetupAccount);
+			});
+		}
+	));
 });
--- src/app/guards/data-unavailable/data-unavailable.guard.spec.ts
+++ src/app/guards/data-unavailable/data-unavailable.guard.spec.ts
@@ -1,15 +1,71 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { inject, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
 
+import { environment } from './../../../environments/environment';
+import { Pages } from './../../common/routing/routesUrls';
 import { DataUnavailableGuard } from './data-unavailable.guard';
 
-describe('DataUnavailableGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DataUnavailableGuard]
-    });
-  });
+@Component({})
+class TestComponent {}
 
-  it('should ...', inject([DataUnavailableGuard], (guard: DataUnavailableGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+describe('DataUnavailableGuard', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [DataUnavailableGuard],
+			imports: [
+				AngularFireModule.initializeApp(environment.firebase),
+				RouterTestingModule.withRoutes([
+					{
+						path: '',
+						component: TestComponent,
+					},
+				]),
+			],
+		});
+	});
+
+	it('should give access to users that did not creat initialization data', inject(
+		[DataUnavailableGuard, UserService, Router],
+		(guard: DataUnavailableGuard, user: UserService) => {
+			spyOnProperty(user, 'hasCreatedData$').and.returnValue(of(false));
+
+			(guard.canActivate(null, null) as Observable<boolean>).subscribe(
+				hasAccess => {
+					expect(hasAccess).toBeTruthy();
+				}
+			);
+		}
+	));
+
+	it('should refuse access to users that created initialization data', inject(
+		[DataUnavailableGuard, UserService, Router],
+		(guard: DataUnavailableGuard, user: UserService) => {
+			spyOnProperty(user, 'hasCreatedData$').and.returnValue(of(true));
+
+			(guard.canActivate(null, null) as Observable<boolean>).subscribe(
+				hasAccess => {
+					expect(hasAccess).toBeFalsy();
+				}
+			);
+		}
+	));
+
+	it('should redirect user to home page if user created initialization data', inject(
+		[DataUnavailableGuard, UserService, Router],
+		(guard: DataUnavailableGuard, user: UserService, router: Router) => {
+			spyOnProperty(user, 'hasCreatedData$').and.returnValue(of(true));
+			const spy = spyOn(router, 'navigateByUrl');
+
+			(guard.canActivate(null, null) as Observable<boolean>).subscribe(
+				hasAccess => {
+					expect(spy).toHaveBeenCalledWith(Pages.Home);
+				}
+			);
+		}
+	));
 });
