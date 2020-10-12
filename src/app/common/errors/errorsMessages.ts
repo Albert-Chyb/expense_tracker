fbc226f2c2c4bbee65ed25456b07a742d5ee0aaa
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-12 18:33:16 +0200

    Added some comments

--- src/app/common/errors/errorsMessages.ts
+++ src/app/common/errors/errorsMessages.ts
@@ -1,5 +1,20 @@
+/**
+ * Contains errors messages that should be displayed to the user.
+ */
+
 export enum ErrorsMessages {
+	/**
+	 * Associated with `auth/popup-closed-by-user` error
+	 */
 	PopupClosedByUser = 'Za wcześnie zamknięto okno logowania.',
+
+	/**
+	 * Associated with `auth/popup-blocked` error
+	 */
 	PopupBlocked = 'Okno logowania zostało zablokowane przez przeklądarkę.',
+
+	/**
+	 * Associated with `auth/cancelled-popup-request` error
+	 */
 	CancelledPopupRequest = 'Anulowano działanie okna logowania. Tylko jedno okno może być aktywne w tym samym czasie.',
 }
