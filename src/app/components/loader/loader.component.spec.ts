1cfe81d96ee5ba9b3db688ef6cb38da416418132
Fidores <45700014+Fidores@users.noreply.github.com>
2020-10-30 23:23:10 +0100

    Fixed loader class in tests

--- src/app/components/loader/loader.component.spec.ts
+++ src/app/components/loader/loader.component.spec.ts
@@ -25,23 +25,23 @@ describe('LoaderComponent', () => {
 
 	describe('classes', () => {
 		it('should add `loader--fixed` class to loader element when isFixed property is set to true', () => {
-			const de = fixture.debugElement.query(By.css('.loader'));
+			const de = fixture.debugElement.query(By.css('.loader-container'));
 			const el: HTMLElement = de.nativeElement;
 
 			component.isFixed = true;
 			fixture.detectChanges();
 
-			expect(el.classList.contains('loader--fixed')).toBeTruthy();
+			expect(el.classList.contains('loader-container--fixed')).toBeTruthy();
 		});
 
 		it('should add `loader--centered` class to loader element when isCenteredAbsolute property is set to true', () => {
-			const de = fixture.debugElement.query(By.css('.loader'));
+			const de = fixture.debugElement.query(By.css('.loader-container'));
 			const el: HTMLElement = de.nativeElement;
 
 			component.isCenteredAbsolutely = true;
 			fixture.detectChanges();
 
-			expect(el.classList.contains('loader--centered')).toBeTruthy();
+			expect(el.classList.contains('loader-container--centered')).toBeTruthy();
 		});
 	});
 });
