2d12d8e8d18e2592eaaf5184fb2592fa2f049fd7
Fidores <45700014+Fidores@users.noreply.github.com>
2020-12-23 20:17:10 +0100

    Errors are always logged to the console.

--- src/app/common/errors/globalErrorHandler.ts
+++ src/app/common/errors/globalErrorHandler.ts
@@ -1,20 +1,13 @@
 import { ErrorsService } from './../../services/errors/errors.service';
 import { ErrorHandler, Injectable } from '@angular/core';
 
-import { environment } from './../../../environments/environment';
-
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 	constructor(private readonly _errors: ErrorsService) {}
 
 	handleError(error: any): void {
-		if (!environment.production) {
-			console.error(error);
-		} else {
-			this._errors.notifyUser('An error occurred');
-			/*
-                Here, an unhandled error would be automatically reported to some service.
-            */
-		}
+		this._errors.notifyUser('An error occurred');
+
+		console.error(error);
 	}
 }
