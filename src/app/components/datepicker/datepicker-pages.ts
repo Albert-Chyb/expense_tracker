9e17a0408a6b6bc1f7c4ec2e012412538d4564ce
Fidores <45700014+Fidores@users.noreply.github.com>
2021-02-12 22:03:18 +0100

    Added option to choose a date from UI

---
+++ src/app/components/datepicker/datepicker-pages.ts
@@ -0,0 +1,230 @@
+import { DatepickerComponent } from './datepicker/datepicker.component';
+
+export type DatepickerPageName = 'month' | 'chooseYear' | 'chooseMonth';
+
+export interface IDatepickerPage {
+	next(): void;
+	prev(): void;
+	isSelected(cellValue: number | string): boolean;
+	choose(cellValue: number | string): void;
+	name: string;
+	data: any[][];
+	headData: any[];
+	hasHeadData: boolean;
+}
+
+export abstract class DatepickerPage {
+	constructor(
+		public name: DatepickerPageName,
+		protected readonly hostRef: DatepickerComponent
+	) {}
+	/** Data of this page. The data is represented as 2D array. */
+	data = [[]];
+
+	/** Data that describes each column in data property. */
+	headData = [];
+
+	get hasHeadData() {
+		return this.headData.length > 0;
+	}
+
+	/** Function generates next set of data. */
+	next() {}
+
+	/** Function generates previous set of data. */
+	prev() {}
+
+	/**
+	 * Turns 1D array into 2D by grouping data into rows.
+	 * Each row is represented as a array.
+	 * @param array Array to group
+	 * @param rowSize Number of items in a single row
+	 * @param startAt Index of the starting point in the grouped array. (default is 0)
+	 * @param emptyValue Fill blank spaces in a row with this value
+	 * @param equalLastRowSize If there is no enough data in the last row start inserting empty value to complete it.
+	 */
+	protected groupIntoRows<T>(
+		array: T[],
+		rowSize: number,
+		startAt = 0,
+		emptyValue: any,
+		equalLastRowSize: boolean
+	): Array<Array<T>> {
+		if (array.length === 0) return [[]];
+		let index = 0;
+		let row = 0;
+		const newArray: Array<Array<T>> = [[]];
+		const lastRowEmptyCells = rowSize - (array.length % rowSize || rowSize);
+		const endingIndex = equalLastRowSize
+			? array.length + lastRowEmptyCells
+			: array.length;
+
+		while (index < endingIndex) {
+			if (index !== 0 && index % rowSize === 0) newArray[++row] = [];
+
+			const itemToInsert =
+				index < startAt ? emptyValue : array[index] ?? emptyValue;
+
+			newArray[row].push(itemToInsert);
+			index++;
+		}
+
+		return newArray;
+	}
+}
+
+export class MonthPage extends DatepickerPage implements IDatepickerPage {
+	constructor(name: DatepickerPageName, hostRef: DatepickerComponent) {
+		super(name, hostRef);
+		this.data = this._generateMonth();
+	}
+	data: number[][];
+	headData = ['P', 'W', 'Ś', 'C', 'P', 'S', 'N'];
+
+	next() {
+		this.hostRef.month += 1;
+		this.data = this._generateMonth();
+	}
+
+	prev() {
+		this.hostRef.month -= 1;
+		this.data = this._generateMonth();
+	}
+
+	isSelected(day: number) {
+		return day === this.hostRef.day;
+	}
+
+	choose(day: number) {
+		this.hostRef.setNewDate(null, null, day);
+	}
+
+	/**
+	 * Generates 2D array with rows as weeks.
+	 * Each day in a week is placed at its proper index relative to name of the day (Monday, Tuesday...).
+	 * Number 0 means empty place.
+	 */
+	private _generateMonth(): Array<Array<number>> {
+		const numberOfDays = this._numbersOfDays();
+		const firstDayIndex = this._firstDayIndex();
+		const month = new Array(numberOfDays + firstDayIndex).fill(
+			0,
+			0,
+			firstDayIndex
+		);
+
+		for (let i = firstDayIndex; i < numberOfDays + firstDayIndex; i++)
+			month[i] = i - firstDayIndex + 1;
+
+		return this.groupIntoRows(
+			month,
+			this.headData.length,
+			firstDayIndex,
+			0,
+			true
+		);
+	}
+
+	/** Returns number of days in the current month. */
+	private _numbersOfDays() {
+		const date = new Date(this.hostRef.date);
+		date.setMonth(date.getMonth() + 1);
+		date.setDate(0);
+
+		return date.getDate();
+	}
+
+	/** Returns position of the first day in first week */
+	private _firstDayIndex() {
+		return (
+			new Date(
+				this.hostRef.date.getFullYear(),
+				this.hostRef.date.getMonth(),
+				1
+			).getDay() - 1
+		);
+	}
+}
+
+export class ChooseYearPage extends DatepickerPage implements IDatepickerPage {
+	constructor(name: DatepickerPageName, hostRef: DatepickerComponent) {
+		super(name, hostRef);
+		this.data = this._generateYears();
+	}
+	private _startYear: number = this.hostRef.year;
+	private readonly _yearsPerPage = 12;
+	data: number[][];
+
+	next() {
+		this._startYear += this._yearsPerPage;
+		this.data = this._generateYears();
+	}
+
+	prev() {
+		this._startYear -= this._yearsPerPage;
+		this.data = this._generateYears();
+	}
+
+	isSelected(year: number) {
+		return year === this.hostRef.year;
+	}
+
+	choose(year: number) {
+		this.hostRef.year = year;
+	}
+
+	private _generateYears() {
+		const data = [];
+		for (
+			let i = this._startYear;
+			i < this._startYear + this._yearsPerPage;
+			i++
+		) {
+			data.push(i);
+		}
+
+		return this.groupIntoRows(data, 4, 0, 0, false);
+	}
+}
+
+export class ChooseMonthPage extends DatepickerPage implements IDatepickerPage {
+	data = [
+		['Styczeń', 'Luty', 'Marzec', 'Kwiecień'],
+		['Maj', 'Czerwiec', 'Lipiec', 'Sierpień'],
+		['Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+	];
+
+	private readonly _months = [
+		'Styczeń',
+		'Luty',
+		'Marzec',
+		'Kwiecień',
+		'Maj',
+		'Czerwiec',
+		'Lipiec',
+		'Sierpień',
+		'Wrzesień',
+		'Październik',
+		'Listopad',
+		'Grudzień',
+	];
+
+	isSelected(month: string) {
+		const monthIndex = this._getMonthIndex(month);
+		return monthIndex === this.hostRef.month;
+	}
+
+	choose(month: string) {
+		this.hostRef.month = this._getMonthIndex(month);
+	}
+
+	private _getMonthIndex(month: string) {
+		return this._months.indexOf(month);
+	}
+}
+
+export const datepickerPages = new Map<DatepickerPageName, any>([
+	['month', MonthPage],
+	['chooseYear', ChooseYearPage],
+	['chooseMonth', ChooseMonthPage],
+]);
--- src/app/components/datepicker/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker/datepicker.component.ts
@@ -1,211 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
-type DatepickerPageName = 'month' | 'chooseYear' | 'chooseMonth';
-interface IDatepickerPage {
-	next(): void;
-	prev(): void;
-	name: string;
-	data: any[][];
-	headData: any[];
-	hasHeadData: boolean;
-}
-
-abstract class DatepickerPage {
-	constructor(
-		public name: DatepickerPageName,
-		protected readonly hostRef: DatepickerComponent
-	) {}
-	/** Data of this page. The data is represented as 2D array. */
-	data = [[]];
-
-	/** Data that describes each column in data property. */
-	headData = [];
-
-	get hasHeadData() {
-		return this.headData.length > 0;
-	}
-
-	/** Function generates next set of data. */
-	next() {}
-
-	/** Function generates previous set of data. */
-	prev() {}
-
-	/**
-	 * Turns 1D array into 2D by grouping data into rows.
-	 * Each row is represented as a array.
-	 * @param array Array to group
-	 * @param rowSize Number of items in a single row
-	 * @param startAt Index of the starting point in the grouped array. (default is 0)
-	 * @param emptyValue Fill blank spaces in a row with this value
-	 * @param equalLastRowSize If there is no enough data in the last row start inserting empty value to complete it.
-	 */
-	protected groupIntoRows<T>(
-		array: T[],
-		rowSize: number,
-		startAt = 0,
-		emptyValue: any,
-		equalLastRowSize: boolean
-	): Array<Array<T>> {
-		if (array.length === 0) return [[]];
-		let index = 0;
-		let row = 0;
-		const newArray: Array<Array<T>> = [[]];
-		const lastRowEmptyCells = rowSize - (array.length % rowSize || rowSize);
-		const endingIndex = equalLastRowSize
-			? array.length + lastRowEmptyCells
-			: array.length;
-
-		while (index < endingIndex) {
-			if (index !== 0 && index % rowSize === 0) newArray[++row] = [];
-
-			const itemToInsert =
-				index < startAt ? emptyValue : array[index] ?? emptyValue;
-
-			newArray[row].push(itemToInsert);
-			index++;
-		}
-
-		return newArray;
-	}
-}
-
-class MonthPage extends DatepickerPage implements IDatepickerPage {
-	constructor(name: DatepickerPageName, hostRef: DatepickerComponent) {
-		super(name, hostRef);
-		this.data = this._generateMonth();
-	}
-	data: number[][];
-	headData = ['P', 'W', 'Ś', 'C', 'P', 'S', 'N'];
-
-	next() {
-		this.hostRef.nextMonth();
-		this.data = this._generateMonth();
-	}
-
-	prev() {
-		this.hostRef.prevMonth();
-		this.data = this._generateMonth();
-	}
-
-	/**
-	 * Generates 2D array with rows as weeks.
-	 * Each day in a week is placed at its proper index relative to name of the day (Monday, Tuesday...).
-	 * Number 0 means empty place.
-	 */
-	private _generateMonth(): Array<Array<number>> {
-		const numberOfDays = this._numbersOfDays();
-		const firstDayIndex = this._firstDayIndex();
-		const month = new Array(numberOfDays + firstDayIndex).fill(
-			0,
-			0,
-			firstDayIndex
-		);
-
-		for (let i = firstDayIndex; i < numberOfDays + firstDayIndex; i++)
-			month[i] = i - firstDayIndex + 1;
-
-		return this.groupIntoRows(
-			month,
-			this.headData.length,
-			firstDayIndex,
-			0,
-			true
-		);
-	}
-
-	/** Returns number of days in the current month. */
-	private _numbersOfDays() {
-		const date = new Date(this.hostRef.date);
-		date.setMonth(date.getMonth() + 1);
-		date.setDate(0);
-
-		return date.getDate();
-	}
-
-	/** Returns position of the first day in first week */
-	private _firstDayIndex() {
-		return (
-			new Date(
-				this.hostRef.date.getFullYear(),
-				this.hostRef.date.getMonth(),
-				1
-			).getDay() - 1
-		);
-	}
-}
-
-class ChooseYearPage extends DatepickerPage implements IDatepickerPage {
-	constructor(name: DatepickerPageName, hostRef: DatepickerComponent) {
-		super(name, hostRef);
-		this.data = this._generateYears();
-	}
-	private _startYear: number = this.hostRef.year;
-	private readonly _yearsPerPage = 12;
-	data: number[][];
-
-	next() {
-		this._startYear += this._yearsPerPage;
-		this.data = this._generateYears();
-	}
-
-	prev() {
-		this._startYear -= this._yearsPerPage;
-		this.data = this._generateYears();
-	}
-
-	private _generateYears() {
-		const data = [];
-		for (
-			let i = this._startYear;
-			i < this._startYear + this._yearsPerPage;
-			i++
-		) {
-			data.push(i);
-		}
-
-		return this.groupIntoRows(data, 4, 0, 0, false);
-	}
-}
-
-class ChooseMonthPage extends DatepickerPage implements IDatepickerPage {
-	data = [
-		['Styczeń', 'Luty', 'Marzec', 'Kwiecień'],
-		['Maj', 'Czerwiec', 'Lipiec', 'Sierpień'],
-		['Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
-	];
-}
-
-const datepickerPages = new Map<DatepickerPageName, any>([
-	['month', MonthPage],
-	['chooseYear', ChooseYearPage],
-	['chooseMonth', ChooseMonthPage],
-]);
+import {
+	IDatepickerPage,
+	MonthPage,
+	DatepickerPageName,
+	datepickerPages,
+} from '../datepicker-pages';
 
 @Component({
 	selector: 'datepicker',
 	templateUrl: './datepicker.component.html',
 	styleUrls: ['./datepicker.component.scss'],
 })
-export class DatepickerComponent implements OnInit {
-	constructor() {}
+export class DatepickerComponent {
+	date = new Date(new Date().setHours(1, 0, 0, 0));
+	page: IDatepickerPage = new MonthPage('month', this);
 
-	date = new Date();
-	currentPage: IDatepickerPage = new MonthPage('month', this);
-
-	ngOnInit(): void {}
+	private readonly _routesOrder = {
+		month: 'month',
+		chooseYear: 'chooseMonth',
+		chooseMonth: 'month',
+	};
 
 	/**
 	 * Changes the page that is currently visible.
 	 * @param pageName Name of the page to switch to.
 	 */
 	switchPage(pageName: DatepickerPageName) {
-		this.currentPage = new (datepickerPages.get(pageName))(pageName, this);
+		if (pageName === this.page.name) return;
+
+		this.page = new (datepickerPages.get(pageName))(pageName, this);
 	}
 
 	/** Function that is called when the button with date is clicked. */
 	onMainBtnClick() {
-		this.currentPage.name === 'month'
+		this.page.name === 'month'
 			? this.switchPage('chooseYear')
 			: this.switchPage('month');
 	}
@@ -223,42 +51,59 @@ export class DatepickerComponent implements OnInit {
 		const month = newMonth ?? this.month;
 		const day = newDay ?? this.day;
 
-		this.date = new Date(year, month, day, 0, 0, 0, 0);
-	}
-
-	/** Switches to the next month. */
-	nextMonth() {
-		this.setNewDate(null, this.month + 1, null);
-	}
-
-	/** Switches to the previous month. */
-	prevMonth() {
-		this.setNewDate(null, this.month - 1, null);
+		this.date = new Date(year, month, day, 1, 0, 0, 0);
 	}
 
 	/** Function that is invoked when next button in the template is clicked. */
 	next() {
-		this.currentPage.next();
+		this.page.next();
 	}
 
 	/** Function that is invoked when previous button the in template is clicked. */
 	prev() {
-		this.currentPage.prev();
+		this.page.prev();
+	}
+
+	/**
+	 * Function that is called when user clicked a cell.
+	 * @param cellValue Value of the clicked cell.
+	 */
+	onCellClick(cellValue: number) {
+		this.page.choose(cellValue);
+
+		this.switchPage(this._routesOrder[this.page.name]);
+	}
+
+	/**
+	 * Determines if passed cell value is currently selected.
+	 * @param cellValue Value of a cell.
+	 */
+	isSelected(cellValue: number) {
+		return this.page.isSelected(cellValue);
 	}
 
 	/** Returns year */
 	get year(): number {
 		return this.date.getFullYear();
 	}
+	set year(newYear: number) {
+		this.setNewDate(newYear, null, null);
+	}
 
 	/** Returns month */
 	get month(): number {
 		return this.date.getMonth();
 	}
+	set month(newMonth: number) {
+		this.setNewDate(null, newMonth, null);
+	}
 
 	/** Returns day of the month */
 	get day(): number {
 		return this.date.getDate();
+	}
+	set day(newDay: number) {
+		this.setNewDate(null, null, newDay);
 	}
 
 	/** Returns day of the week */
