313ce01a7d993e4e9dcd6b701cba5b8b9d138a71
Fidores <45700014+Fidores@users.noreply.github.com>
2021-02-18 22:34:12 +0100

    Added prefix component that can be placed inside form field

---
+++ src/app/components/form-field/form-field-prefix.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'form-field-prefix',
+	template: '<ng-content></ng-content>',
+	styles: [],
+})
+export class FormFieldPrefix {}
--- src/app/components/form-field/form-field.module.ts
+++ src/app/components/form-field/form-field.module.ts
@@ -1,3 +1,4 @@
+import { FormFieldPrefix } from './form-field-prefix.component';
 import { FormFieldLabelComponent } from './form-field-label/form-field-label.component';
 import { FormFieldComponent } from './form-field/form-field.component';
 import { FormFieldInputDirective } from './form-field-input.directive';
@@ -9,12 +10,14 @@ import { CommonModule } from '@angular/common';
 		FormFieldInputDirective,
 		FormFieldComponent,
 		FormFieldLabelComponent,
+		FormFieldPrefix,
 	],
 	imports: [CommonModule],
 	exports: [
 		FormFieldInputDirective,
 		FormFieldComponent,
 		FormFieldLabelComponent,
+		FormFieldPrefix,
 	],
 })
 export class FormFieldModule {}
