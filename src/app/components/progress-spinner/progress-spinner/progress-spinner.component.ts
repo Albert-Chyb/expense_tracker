55a9efd70f59f3c80188216b7cef6a09e11f325f
Fidores <45700014+Fidores@users.noreply.github.com>
2021-05-31 23:14:43 +0200

    Added comments

--- src/app/components/progress-spinner/progress-spinner/progress-spinner.component.ts
+++ src/app/components/progress-spinner/progress-spinner/progress-spinner.component.ts
@@ -14,11 +14,19 @@ type TProgressSpinnerMode = 'progress' | 'infinity';
 export class ProgressSpinnerComponent {
 	private _radius = 45;
 
+	/** Mode of the progress spinner. It can be either progress or infinity. */
 	@Input('mode') mode: TProgressSpinnerMode = 'infinity';
+
+	/** When spinner is in the progress mode, this value represents 100%. */
 	@Input('max') maxValue = 100;
-	@Input('diagonal') diagonal: number = 120;
+
+	/** When spinner is in the progress mode, this is the current progress of an action. */
 	@Input('value') value = 45;
 
+	/** Sets diagonal (width) of the spinner. */
+	@Input('diagonal') diagonal: number = 120;
+
+	/** Sets radius of the inner circle. The lesser the radius, the bolder the border width. */
 	@Input('radius')
 	set radius(value: number) {
 		this._radius = ~~this._limitToRange(value, 0, 50);
@@ -36,13 +44,12 @@ export class ProgressSpinnerComponent {
 	}
 
 	get strokeDashoffset(): number {
-		let offset: number;
+		let offset: number = this.strokeDasharray;
 
 		if (this.mode === 'infinity') {
-			offset = this.strokeDasharray * 0.25;
+			offset *= 0.25;
 		} else {
-			offset =
-				this.strokeDasharray * Math.min(1 - this.value / this.maxValue, 1);
+			offset *= Math.min(1 - this.value / this.maxValue, 1);
 		}
 
 		return Math.abs(~~offset);
