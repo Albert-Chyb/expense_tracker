440fd61fc0797aece7ea0e45ed32d79fb001f4f4
Fidores <45700014+Fidores@users.noreply.github.com>
2021-06-01 22:15:18 +0200

    Code cleanup

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,22 +1,7 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	templateUrl: './not-found.component.html',
 	styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
-	constructor(private readonly _changeDetector: ChangeDetectorRef) {}
-
-	mode: string = 'progress';
-	value: number = 20;
-	maxValue: number = 100;
-	displayProgress = true;
-
-	ngOnInit() {
-		// this._changeDetector.detach();
-
-		setTimeout(() => {
-			this.value = 80;
-		}, 2000);
-	}
-}
+export class NotFoundComponent {}
